6836c62f8aebec46320b43b5b8c88015674bf7f5
eli <miaoyu2009@gmail.com>
2024-04-28 16:33:33 +0800

    ignore

--- core/utils.js
+++ core/utils.js
@@ -13,11 +13,22 @@ function removeWhitespace(str) {
 function printGreen(message) {
     console.log('\x1b[32m%s\x1b[0m', message);
 }
+// 打印黄色文本
+function printYellow(text) {
+    console.log('\x1b[33m' + text + '\x1b[0m');
+}
+
+// 打印红色文本
+function printRed(text) {
+    console.log('\x1b[31m' + text + '\x1b[0m');
+}
 
 const utils = {
     getFullPath,
     removeWhitespace,
-    printGreen
+    printGreen,
+    printYellow,
+    printRed
 }
 
 module.exports = utils
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 const utils = require('./core/utils')
-console.log('Hello, CLI world!');
-// const chalk = require('chalk');  
+const http = require('http');
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+// const bossConfig = require('./boss.config')
+const {getPageHtml} =require('./core')
 
 
 utils.printGreen(
@@ -14,9 +22,59 @@ utils.printGreen(
 program
   .version('1.0.0')
   .description('boss web server')
-  .option('-d, --dev', 'start server for development')
+//   .option('-d, --dev', 'start server for development')
   .parse(process.argv);
 
-if (program.dev) {
-  console.log('Hello, CLI world!');
-}
+// if (program.dev) {
+//   console.log('Hello, CLI world!');
+// }
+
+const PORT = bossConfig.port || 8888;
+const pages = bossConfig.pages
+
+
+const server = http.createServer(async (req, res) => {
+    // console.log('req.url', req.url)
+    const config = pages.find(item => item.router === req.url)
+    console.log('req.url', req.url)
+    if (req.url === '/' || req.url === '/index.html') {
+        // 读取本地的index.html文件并发送给客户端
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+      } 
+    // 如果是/weex开头的请求，访问当前路径下的assets文件夹并返回js文件
+    else if (req.url.startsWith('/weex')) {
+
+      const jsFilePath = path.join(__dirname, req.url.replace('/weex', ''));
+      // console.log(jsFilePath)
+      fs.readFile(jsFilePath, (err, data) => {
+          if (err) {
+              res.writeHead(404, {'Content-Type': 'text/plain'});
+              res.end('Not Found');
+              return;
+          }
+          res.writeHead(200, {'Content-Type': 'application/javascript'});
+          res.end(data);
+      });
+    } 
+    else if (config) {
+      const htmlStr = await getPageHtml(config.page)
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(htmlStr);
+  } else {
+        // 转发请求到指定的目标地址
+        const targetUrl = 'https://stg-boss-web.weex.tech' + req.url;
+        req.pipe(request(targetUrl)).pipe(res);
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
